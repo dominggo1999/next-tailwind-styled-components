a052cbe22c73283081a7dfa0092f7ed1fc4454ec
dominggo1999 <arnolddominggo19@gmail.com>
2022-02-11 17:18:15 +0700

    Init

---
+++ components/Test.jsx
@@ -0,0 +1,12 @@
+import tw from 'tailwind-styled-components';
+
+export const Box = tw.div`
+  w-full 
+  h-screen 
+  bg-black 
+  flex 
+  justify-center
+  items-center
+  text-white
+  text-2xl
+`;
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import { Box } from '../components/Test';
+
+const Home = () => {
+  return (
+    <Box>
+      Placeholder
+    </Box>
+  );
+};
+
+export default Home;
